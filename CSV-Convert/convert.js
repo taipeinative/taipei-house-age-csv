d4c6c0a5a0da824d6e804ddff41cf6faeab31ebc
Taipei Native <85215798+taipeinative@users.noreply.github.com>
2022-03-14 22:50:35 +0800

    beta 0.2.1 - Fixed Minor Locale Problem
    The update fixed the problem that the text indicating 'end of the preview' and characters' count were unable to translate into Traditional Chinese. For other improvements, I quit using anonymous function for 'info-button', since I need it to be reuse (become 'next' button).

--- CSV-Convert/convert.js
+++ CSV-Convert/convert.js
@@ -1,8 +1,15 @@
 // Pre-defined variables.
-var previewText = '';
-var isLonger = 0;
-var language = 'en';
 var currentState = 'upload';
+var language = 'en';
+var previewText = '';
+var codeLength;
+var previewLength;
+var fromCode = 0;
+var toCode = 9999;
+var fileManager;
+var xml;
+var isLonger = 0;
+
 
 
 
@@ -34,18 +41,7 @@ function clickUpload() {
 
   var info = document.getElementById('info-button');
   var upload = document.getElementById('upload-button');
-
-  info.addEventListener("click", function (e) {
-
-    if (upload) {
-
-      upload.click();
-
-    }
-
-    e.preventDefault();
-
-  }, false);
+  info.addEventListener("click", uploadListener , false);
 
 }
 
@@ -62,7 +58,7 @@ function clickUpload() {
 function readfile() {
 
   var file = this.files[0];
-  var fileManager = new FileReader();
+  fileManager = new FileReader();
 
   fileManager.onloadstart = function (event) {
 
@@ -76,11 +72,15 @@ function readfile() {
   fileManager.onload = function (event) {
 
     currentState = 'preview';
-    previewText = codeRestorer(codeSlicer(fileManager.result,0,9999));
+    codeLength = fileManager.result.length;
+    previewLength = codeLength - ( toCode - fromCode );
+    xml = codeSlicer(fileManager.result,fromCode,toCode);
+    previewText = codeRestorer(xml,'default');
     document.getElementById('preview-text').innerHTML = previewText;
     message();
     statusUpdate();
     localeUpdate();
+    document.getElementById('info-button').removeEventListener("click", uploadListener , false);
 
   };
 
@@ -102,17 +102,28 @@ function readfile() {
 // Slice .xml files in order to speed up preview time.
 function codeSlicer (code,from,to) {
 
-  var codeLength = code.length;
   var result = code.slice(from,to);
-  if ( codeLength > ( to - from) ) {
+
+  if ( previewLength > 0 ) {
 
     isLonger = 0;
-    return `${result + '^' + ( codeLength - ( to - from ) ) + '%' }`;
+    return result;
 
   } else {
 
     isLonger = 1;
-    return `${result} <br /><span class = "continue">(End of the preview)</span>`;
+
+    switch (language) {
+
+      case 'en' :
+        return `${result} ${locale[14].en}`;
+        break;
+
+      case 'zh' :
+        return `${result} ${locale[14].zh}`;
+        break;
+
+    }
 
   }
 
@@ -133,11 +144,59 @@ function codeSlicer (code,from,to) {
     Output: I^m feelin' 100% good. <br /><span class = "continue">(End of the preview)<br />( 1111 characters behind)</span>
 
 */
-function codeRestorer (code) {
+function codeRestorer (code,tag) {
 
   var result = code.replace(/<(?!br|span|\/span)/g,'&lt;').replace(/\n/g,'<br />').replace(/\t/g,'&ensp;&ensp;');
-  result = ( (isLonger) ? (result) : ( result.replace(/\^\d+%/,'<br /><span class = "continue">(End of the preview)<br />(') + result.match(/(?<=\^)\d+(?=%)/) + ' characters behind)</span>') );
+
+  switch (tag) {
+
+    case 'default':
+
+      switch (language) {
+
+        case 'en':
+          result = ( (isLonger) ? (result) : ( result + locale[15].en + previewLength + locale[16].en ) );
+          break;
+
+        case 'zh':
+          result = ( (isLonger) ? (result) : ( result + locale[15].zh + previewLength + locale[16].zh ) );
+          break;
+
+      }
+      break;
+
+    case 'replace':
+
+      switch (language) {
+
+        case 'en':
+          result = ( (isLonger) ? (`${fileManager.result.slice(fromCode,toCode).replace(/<(?!br|span|\/span)/g,'&lt;').replace(/\n/g,'<br />').replace(/\t/g,'&ensp;&ensp;')} ${locale[14].en}`) : (xml + locale[15].en + previewLength + locale[16].en) );
+          break;
+
+        case 'zh':
+          result = ( (isLonger) ? (`${fileManager.result.slice(fromCode,toCode).replace(/<(?!br|span|\/span)/g,'&lt;').replace(/\n/g,'<br />').replace(/\t/g,'&ensp;&ensp;')} ${locale[14].zh}`) : (xml + locale[15].zh + previewLength + locale[16].zh) );
+          break;
+
+      }
+      break;
+
+  }
 
   return result;
+
+}
+
+
+
+// Listener implemented on 'Upload' button.
+function uploadListener (e) {
+
+  if (document.getElementById('upload-button')) {
+
+    document.getElementById('upload-button').click();
+
+  }
+
+  e.preventDefault();
 
 }
--- CSV-Convert/locale.js
+++ CSV-Convert/locale.js
@@ -51,20 +51,33 @@ function localeUpdate() {
 
   for ( var i = 0; i < locale.length; i++ ) {
 
+    var dom = document.getElementById(locale[i].id);
     var obj = locale[i];
     var objState = JSON.stringify(obj.state);
     var objRegEx = new RegExp( `${currentState}(?= |")` , 'g');
+    var prvRegEx = /<br><span class=\"continue\">\(End of the preview\)<br>\(\d+characters behind\)<\/span>|<br><span class=\"continue\">（預覽結束）<br>（尚有\d+個字元未顯示）<\/span>/;
 
     if ( ( ( objState.search(objRegEx) != -1 ) ? 1 : 0) || ( objState.includes('all') ) ) {
 
       switch ( language ) {
 
         case 'en':
-          document.getElementById(locale[i].id).innerHTML = locale[i].en;
+          dom.innerHTML = locale[i].en;
           break;
 
         case 'zh':
-          document.getElementById(locale[i].id).innerHTML = locale[i].zh;
+          dom.innerHTML = locale[i].zh;
+          break;
+
+      }
+
+    } else if ( ( ( objState.search('special') != -1 ) ? 1 : 0) ) {
+
+      switch ( currentState ) {
+
+        case 'preview':
+
+          dom.innerHTML = codeRestorer(dom.innerHTML,'replace');
           break;
 
       }
